8f342028cb70f62c26938d66696aaeb9c04f878b
heesane <tees3359@gmail.com>
2024-07-08 12:46:56 +0900

    Loan Result Dto

---
+++ css-module/src/main/java/com/zerobase/css/dto/LoanResultDto.java
@@ -0,0 +1,18 @@
+package com.zerobase.css.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+public class LoanResultDto {
+
+    @Getter
+    @AllArgsConstructor
+    @ToString
+    public static class ResponseDto{
+        String userKey;
+        Long limitAmount;
+        Double interest;
+    }
+}
