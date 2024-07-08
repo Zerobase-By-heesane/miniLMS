f7c434b5f47ffaca939c9b7b28594459373488f3
heesane <tees3359@gmail.com>
2024-07-08 12:46:32 +0900

    Loan Review Dto

--- api-module/src/main/java/com/zerobase/loan/review/LoanReviewController.java
+++ api-module/src/main/java/com/zerobase/loan/review/LoanReviewController.java
@@ -1,6 +1,8 @@
 package com.zerobase.loan.review;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,17 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/fintech/api/v1")
 @RequiredArgsConstructor
 public class LoanReviewController {
 
     private final LoanReviewService loanReviewService;
 
-    @GetMapping("/review/{userKey}")
-    public ResponseEntity<LoanReviewDto.LoanReviewResponseDto> getReviewData(
+    @GetMapping(value = "/review/{userKey}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public LoanReviewDto.LoanReviewResponseDto getReviewData(
             @PathVariable String userKey
     ) {
-        return ResponseEntity.ok(loanReviewService.loanReviewMain(userKey));
+        LoanReviewDto.LoanReviewResponseDto loanReviewResponseDto = loanReviewService.loanReviewMain(userKey);
+        log.info("loanReviewResponseDto : {}", loanReviewResponseDto);
+        return loanReviewResponseDto;
 
     }
 }
