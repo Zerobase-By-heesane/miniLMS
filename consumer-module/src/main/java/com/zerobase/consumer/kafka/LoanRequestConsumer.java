e7ae56b6e90e27bcb7a8d7fa8e31cecf53e76d3e
heesane <tees3359@gmail.com>
2024-07-08 12:47:10 +0900

    Loan Request Consumer

---
+++ consumer-module/src/main/java/com/zerobase/consumer/kafka/LoanRequestConsumer.java
@@ -0,0 +1,26 @@
+package com.zerobase.consumer.kafka;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zerobase.consumer.service.LoanRequestService;
+import com.zerobase.kafka.dto.LoanRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class LoanRequestConsumer {
+
+    private final ObjectMapper objectMapper;
+    private final LoanRequestService loanRequestService;
+
+    @KafkaListener(topics = "loan-request", groupId = "fintech")
+    public void consume(String message) throws JsonProcessingException {
+        log.info("Consumed message: {}", message);
+        LoanRequestDto loanRequestDto = objectMapper.readValue(message, LoanRequestDto.class);
+        loanRequestService.loanRequest(loanRequestDto);
+    }
+}
