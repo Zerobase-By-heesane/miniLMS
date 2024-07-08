7d1a439e7642ef52a65cac291928692d98874cb1
heesane <tees3359@gmail.com>
2024-07-08 12:47:04 +0900

    Loan Request Dto

---
+++ css-module/src/main/java/com/zerobase/css/dto/LoanRequestDto.java
@@ -0,0 +1,20 @@
+package com.zerobase.css.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+public class LoanRequestDto {
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @ToString
+    public static class RequestInputDto{
+        String userKey;
+        String userName;
+        Long userIncomeAmount;
+        String userRegistrationNumber;
+    }
+}
