87c5f1e8adf7f624f8fbafea8ae3af2e41d46f7c
heesane <tees3359@gmail.com>
2024-07-08 12:46:41 +0900

    Loan Review Dto

--- api-module/src/main/java/com/zerobase/loan/review/LoanReviewDto.java
+++ api-module/src/main/java/com/zerobase/loan/review/LoanReviewDto.java
@@ -1,20 +1,28 @@
 package com.zerobase.loan.review;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+@AllArgsConstructor
+@Getter
 public class LoanReviewDto {
 
     @AllArgsConstructor
     @NoArgsConstructor
+    @Getter
     @Builder
     public static class LoanReviewResponseDto{
+
+        @JsonProperty("userKey")
         String userKey;
+        @JsonProperty("loanResult")
         LoanResult loanResult;
 
     }
     @AllArgsConstructor
     @NoArgsConstructor
+    @Getter
     @Builder
     public static class LoanResult{
         Long userLimitAmount;
